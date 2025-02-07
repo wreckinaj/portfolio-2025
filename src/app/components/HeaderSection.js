ce78f01de50dd993fc50346c870e593a83d09b06
Anthony Silva <ajsilva228@outlook.com>
2025-02-07 13:18:04 -0800

    Added header section

---
+++ src/app/components/HeaderSection.js
@@ -0,0 +1,74 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
+const HeaderSection = () => {
+    return(
+        <section>
+            <div className='grid grid-cols-1 sm:grid-cols-12'>
+                <div className='col-span-7 place-self-center text-center sm:text-left'>
+                    <h1 className='text-red-600 mb-4 sm:text-5xl text-4xl lg:text-6xl font-bold'>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-pink-600'>
+                            Hello, I'm Anthony,
+                        </span>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Software Engineer.',
+                                1000,
+                                'Backend Developer.',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'AI Fanatic.',
+                                1000,
+                                'Swimmer.',
+                                1000,
+                                'Geography Lover.',
+                                1000,
+                                'Leader.',
+                                1000,
+                                'Problem Solver.',
+                                1000,
+                                'a person on the spectrum.',
+                                1000,
+                                'willing to make a difference.',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            style={{ fontSize: '1em', display: 'inline-block' }}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className='text-zinc-400 text-base sm:text-lg mb-6 lg:text-xl'>
+                        Changing coding to change the world!
+                    </p>
+                    <div>
+                        <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-red-500 via-pink-500 to-purple-500 hover:bg-slate-200 text-white'>
+                            Connect With Me
+                        </button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-red-500 via-pink-500 to-purple-500 hover:bg-slate-800 text-white border mt-3'>
+                            <span className='block bg-sky-800 hover:bg-slate-800 rounded-full px-5 py-2'>
+                                My Resume
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                    <div className='rounded-full bg-sky-700 w-[350px] h-[350px] lg:w-[400px] lg:h-[400px] relative'>
+                        <Image
+                            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                            src="/anthony.jpg"
+                            alt="Anthony's image"
+                            width={200}
+                            height={200}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeaderSection;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import HeaderSection from "./components/HeaderSection"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Anthony Silva's Territory</h1>
-    </div>
+    <main className="flex min-h-screen flex-col bg-sky-800">
+      <div className='container mx-auto px-12 py-4'>
+        <HeaderSection/>
+      </div>
+    </main>
   );
 }
